f5b18a7dab8bfc20d7d3a4c4ceaa4dee5555aeb1
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-08-16 00:48:38 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-import Masonry from "masonry-layout";
-
 window.onload = () => {
 	const grid = document.querySelector(".grid");
 
-	const masonary = new Masonry(grid);
+	const masonary = new Masonry(grid, {
+		itemSelector: ".grid-item",
+		gutter: 10,
+	});
 };
