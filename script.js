89bc766748a90a08e51e214e5b9eaee11557ca89
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-08-16 17:27:21 +0200

    update

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ window.onload = () => {
 		fitWidth: true,
 	});
 };
+
+const galleryItem = document.querySelectorAll(".grid-item");
+
+for (let i = 0; i < galleryItem.length; i++) {
+	galleryItem[i].addEventListener("click", (e) => {
+		let modalImage = document.querySelector(".modalImage");
+		let src = e.target.getAttribute("src");
+		modalImage.src = src;
+
+		const modal = new bootstrap.Modal(
+			document.querySelector("#staticBackdrop")
+		);
+		modal.show();
+	});
+}
